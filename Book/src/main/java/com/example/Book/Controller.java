7aa39f1857506ed75b2b2a03ee1bacccb7cd2151
Bala12333 <bchanadr2005@gmail.com>
2025-08-01 14:29:00 +0000

    update controller

--- Book/src/main/java/com/example/Book/Controller.java
+++ Book/src/main/java/com/example/Book/Controller.java
@@ -2,15 +2,16 @@ package com.example.Book;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.Book.BookService;
+
+import java.util.ArrayList;
 
 @RestController
 public class Controller {
-    @GetMapping("/")
-    public String Welcome(){
-        return  "Hello World";
-    }
-    @GetMapping("/name")
-    public int GetAge(){
-        return 1947;
+    BookService bookSer=new BookService();
+    @GetMapping("/books")
+    public ArrayList<BookDetails> getBooks(){
+
+        return bookSer.getBooks();
     }
 }
