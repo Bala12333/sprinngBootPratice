d9a1d959723bc765e5a7f78942d7d70e450d0fc7
Bala12333 <bchanadr2005@gmail.com>
2025-08-11 06:35:12 +0000

    ffile path changes

---
+++ Book/src/main/java/com/example/Book/Controller/Controller.java
@@ -0,0 +1,40 @@
+package com.example.Book;
+
+
+import org.springframework.web.bind.annotation.*;
+import com.example.Book.BookService;
+import com.example.Book.BookDetails;
+
+import java.util.ArrayList;
+
+@RestController
+public class Controller {
+    BookService bookSer=new BookService();
+    @GetMapping("/books")
+    public ArrayList<BookDetails> getBooks(){
+
+        return bookSer.getBooks();
+    }
+    @GetMapping("/books/{bookId}")
+    public  BookDetails getBookId(@PathVariable("bookId") int id){
+
+        return bookSer.getBookId(id);
+    }
+    @PostMapping("/books")
+    public BookDetails add_book(@RequestBody BookDetails book){
+       return bookSer.add_book(book);
+
+
+
+    }
+    @PutMapping("/books/{bookId}")
+    public BookDetails update_Book(@PathVariable("bookId") int id,@RequestBody BookDetails book){
+        return bookSer.update_Book(id,book);
+    }
+    @DeleteMapping("/books/{bookId}")
+    public void delete_Book(@PathVariable("bookId") int id){
+        bookSer.delete_Book(id);
+
+
+    }
+}
---
+++ Book/src/main/java/com/example/Book/Service/BookService.java
@@ -0,0 +1,81 @@
+package com.example.Book;
+
+import java.util.*;
+import com.example.Book.BookDetails;
+import com.example.Book.BookRepo;
+import org.springframework.web.server.ResponseStatusException;
+import  org.springframework.http.HttpStatus;
+
+
+
+public class BookService implements BookRepo{
+    int uniId=104;
+    private HashMap<Integer,BookDetails> bookMap=new HashMap<>();
+    public BookService(){
+        BookDetails b1=new BookDetails(101,"Psychology of Money","Psychology_of_Money.png");
+        BookDetails b2=new BookDetails(102,"Rich Dad Poor Dad","Rich_Dad_Poor_Dad.jpg");
+        BookDetails b3=new BookDetails(103,"Your Money or Your Life","Your_Money_or_Your_Life.jpg");
+        bookMap.put(b1.getId(),b1);
+        bookMap.put(b2.getId(),b2);
+        bookMap.put(b3.getId(),b3);
+
+    }
+    @Override
+    public ArrayList<BookDetails>  getBooks(){
+        Collection<BookDetails> bookDetailsConnection=bookMap.values();
+        ArrayList<BookDetails> books=new ArrayList<>(bookDetailsConnection);
+        return  books;
+
+    }
+    @Override
+    public BookDetails getBookId(int id){
+        BookDetails book=bookMap.get(id);
+        if (book==null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return book;
+    }
+    @Override
+    public  BookDetails add_book(BookDetails book){
+        book.setId(uniId);
+        bookMap.put(uniId,book);
+        uniId+=1;
+
+        return book;
+    }
+
+    @Override
+    public BookDetails update_Book(int id, BookDetails book) {
+        BookDetails exist_Book=bookMap.get(id);
+        if (exist_Book==null){
+            throw new ResponseStatusException((HttpStatus.NOT_FOUND));
+        }
+        if (book.getImageUrl()!=null){
+            exist_Book.setImageUrl(book.getImageUrl());
+
+        }
+        if (book.getName()!=null){
+            exist_Book.setName(book.getName());
+
+
+        }
+
+        return exist_Book;
+
+    }
+
+    @Override
+    public void delete_Book(int id) {
+        BookDetails book=bookMap.get(id);
+        if (book==null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        }
+        else {
+            bookMap.remove(id);
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+        }
+    }
+
+
+}
